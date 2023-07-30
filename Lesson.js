ca9a99537f67f52854ee0bacc2c8d85c65be62ed
Tatyana <tabk@bk.ru>
2023-07-30 20:09:39 +0300

    add file lesson

---
+++ Lesson.js
@@ -0,0 +1,18 @@
+var myFirstName = 'Tatyana';
+ver myLastName = 'Yamatina';
+/*Это мой учебный файл*/
+//Undestanding Case Sensivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+/*Замена ключевых слов var на let. Ключевое слово let может объявить переменную один раз*/
+let catName = "Oliver";
+let catSound = "Meow!";
+
