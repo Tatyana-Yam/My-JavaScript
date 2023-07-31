5017fa1d35462706268e3b29d102bf6ddb1d2872
Tatyana <tabk@bk.ru>
2023-07-31 10:25:30 +0300

    add calculaitions

--- Lesson.js
+++ Lesson.js
@@ -12,7 +12,48 @@ studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
 
-/*Замена ключевых слов var на let. Ключевое слово let может объявить переменную один раз*/
+/*Замена ключевых слов var на let. Ключевое слово let 
+может объявить переменную один раз*/
 let catName = "Oliver";
 let catSound = "Meow!";
 
+//Объявить переменную только для чтения
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//Оператор сложения - addition operator
+const sum = 10 + 10;
+console.log(sum);
+
+//Оператор вычитания - symbol for subtraction
+const difference = 45 - 33;
+console.log(difference);
+
+//Оператор умножения - symbol for multiply
+const product = 8 * 10;
+console.log(product);
+
+//Оператор деления - symbol for division
+const quotient = 66 / 33;
+console.log(quotient);
+
+//Incrtment - увеличение на 1, не используя оператор присваивания (сложения)
+let myVar = 87;
+myVar++;
+console.log(myVar);
+
+//Decrement - уменьшение на 1, не используя оператор вычитания
+let myVar = 11;
+myVar--;
+console.log(myVar);
+
+//Desimals - десятичные числа
+const myDecimal = 5.7
+
+const product = 2.0 * 2.5;
+console.log(product);
+
+const quotient = 4.4 / 2.0;
+console.log(quotient);
