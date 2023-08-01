b30a74ae5d01a6cfb7c93ffd9779988230f748d0
Tatyana <tabk@bk.ru>
2023-08-01 21:33:30 +0300

    add lesson quontes

---
+++ Lesson 1.js
@@ -0,0 +1,25 @@
+//Finding a Remainder Нахождение остатка
+const remainder = 11 % 3;
+console.log(remainder);
+
+
+a += 12;
+b += 9;
+c += 7;
+console.log(a, b, c)
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+console.log(myStr)
+
